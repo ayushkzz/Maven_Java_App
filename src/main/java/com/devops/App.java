fa58bbf4e7a3c214313c9c302ddc4b905a2d3a61
Ayush Kumar <146207142+ayushkzz@users.noreply.github.com>
2026-05-17 04:01:04 +0530

    Update App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -8,14 +8,10 @@ public class App {
     public String greet() {
         return "CI/CD Pipeline Successful";
     }
-    // public static void main(String[] args) {
-    //     App app = new App();
-    //     System.out.println(app.greet());
-    // }
+
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        // ✅ Frontend (HTML page)
         server.createContext("/", exchange -> {
             String html = "<html><body><h1>CI/CD Pipeline Demo</h1></body></html>";
 
